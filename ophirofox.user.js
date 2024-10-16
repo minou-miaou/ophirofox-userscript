b09321f8b2f6487c30e05a5582bc484b543e44a2
minou-miaou <lesne.aymeric@gmail.com>
2024-10-16 14:05:41 +0200

    Update ophirofox.user.js

--- ophirofox.user.js
+++ ophirofox.user.js
@@ -1,7 +1,9 @@
 // ==UserScript==
+// @name    OphirofoxScript
 // @version 2.4.26371.34
 // @author  Write
-// @name    OphirofoxScript
+// @downloadURL	 https://github.com/minou-miaou/ophirofox-userscript/raw/refs/heads/main/ophirofox.user.js
+// @updateURL	 https://github.com/minou-miaou/ophirofox-userscript/raw/refs/heads/main/ophirofox.user.js
 // @grant   GM.getValue
 // @grant   GM.setValue
 // @grant   GM.deleteValue
